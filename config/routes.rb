7207a2d5153a3dd25851015b18207dd55e517b36
unknown <chris@.(none)>
2011-02-26 21:04:47 +0100

    Done with exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 SampleApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
-
   get "pages/about"
+  get "pages/help"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
