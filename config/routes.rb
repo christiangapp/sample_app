9a6067be371eab6fb171501a0c140a8367098d5d
unknown <chris@.(none)>
2011-02-27 16:54:38 +0100

    Chapter 5 finfished

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/signup',    :to => 'users#new'
+  match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
